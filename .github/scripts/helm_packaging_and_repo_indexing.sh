0059d2d1501027eff7943fe08c1ac7c9566937ba
Franck Levesque <franck.levesque@gmail.com>
2024-03-12 23:28:21 +0100

    Removed the 'helm' parent directory.

--- .github/scripts/helm_packaging_and_repo_indexing.sh
+++ .github/scripts/helm_packaging_and_repo_indexing.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-HelmChartsDir="./helm/charts"       # Helm Charts Directory (files)
-HelmPackagesDir="./helm/packages"   # Helm packaged Charts (.tgz)
+HelmChartsDir="./charts"       # Helm Charts Directory (files)
+HelmPackagesDir="./packages"   # Helm packaged Charts (.tgz)
 
 # Package Helm Charts
 ChartsList=$( find ${HelmChartsDir} -type d -maxdepth 1 | grep -v ^\.\.helm\/charts$ )
 for Chart in $( echo ${ChartsList} ); do
+  printf "Packaging the Helm charts ...\n"
   helm package ${Chart} --destination ./helm/packages
 done
 
 # Repo Index
-helm repo index ./helm/packages
+printf "Indexing the Helm charts ...\n"
+helm repo index ${HelmPackagesDir}
